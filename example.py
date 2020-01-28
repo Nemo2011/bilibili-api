991961752171571a73d6e2658d928f8650f754c7
Passkou <psk116@outlook.com>
2020-01-28 13:09:38 +0800

    V1.1.1

--- example.py
+++ example.py
@@ -2,4 +2,4 @@ from bilibili_api import video
 import json
 my_video = video.VideoInfo(aid="40473736")
 info = my_video.get_video_info()
-print(json.dumps(my_video, indent=4, ensure_ascii=False))
+print(json.dumps(info, indent=4, ensure_ascii=False))
