6b739361f66da45e12805f88ffa952d5a4af1ed8
Passkou <45550579+Passkou@users.noreply.github.com>
2020-02-05 19:08:05 +0800

    Create send_dynamic_example.py

---
+++ send_dynamic_example.py
@@ -0,0 +1,25 @@
+from bilibili_api.dynamic import DrawDynamic, TextDynamic, InstantDynamic, UploadImages
+from bilibili_api import Verify
+
+# 验证类
+verify = Verify(sessdata="", csrf="")
+
+# 上传图片路径列表
+img_path = [
+    "pic1", "pic2"
+]
+
+# 上传图片类
+upload = UploadImages(images_path=img_path, verify=verify)
+
+# 画册动态类
+draw = DrawDynamic(text="这是文字内容", upload_images=upload, verify=verify)
+
+# 纯文本动态
+text = TextDynamic(text="这是文字内容", verify=verify)
+
+# 立即发送动态类
+instant = InstantDynamic(draw)
+
+# 发送动态，图片会自动上传
+instant.send()
