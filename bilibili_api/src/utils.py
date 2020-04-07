399c2d1afe062657e7e711a21d424524703a9b52
Passkou <psk116@outlook.com>
2020-04-07 21:57:18 +0800

    V1.3.3

--- bilibili_api/src/utils.py
+++ bilibili_api/src/utils.py
@@ -189,3 +189,42 @@ def nyanpass(这是一个毫无用处的参数="nyanpass"):
     print("喵帕斯(=・ω・=)\nhttp://space.bilibili.com/12440199")
     time.sleep(5)
     os.remove("tmp.mp3")
+
+
+# 代码来源：https://www.zhihu.com/question/381784377/answer/1099438784
+def bv2aid(bv: str):
+    table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+    tr = {}
+    for i in range(58):
+        tr[table[i]] = i
+    s = [11, 10, 3, 8, 4, 6]
+    xor = 177451812
+    add = 8728348608
+
+    def dec(x):
+        r = 0
+        for i in range(6):
+            r += tr[x[s[i]]] * 58 ** i
+        return (r - add) ^ xor
+
+    return dec(bv)
+
+
+# 代码来源：https://www.zhihu.com/question/381784377/answer/1099438784
+def aid2bv(aid: int):
+    table = 'fZodR9XQDSUm21yCkr6zBqiveYah8bt4xsWpHnJE7jL5VG3guMTKNPAwcF'
+    tr = {}
+    for i in range(58):
+        tr[table[i]] = i
+    s = [11, 10, 3, 8, 4, 6]
+    xor = 177451812
+    add = 8728348608
+
+    def enc(x):
+        x = (x ^ xor) + add
+        r = list('BV1  4 1 7  ')
+        for i in range(6):
+            r[s[i]] = table[x // 58 ** i % 58]
+        return ''.join(r)
+
+    return enc(aid)
