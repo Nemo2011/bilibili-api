430487c4c0a91f750ffebc06c0dfe7b19784cfec
Passkou <psk116@outlook.com>
2020-08-28 09:41:29 +0800

    V2.0.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='bilibili_api',
-    version="2.0.2",
+    version="2.0.3",
     license='GPLv3+',
     author='Passkou',
     author_email='psk116@outlook.com',
