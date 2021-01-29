7555add6e97588d195914888bd2fca96ce3735ca
david082321 <7046617+david082321@users.noreply.github.com>
2021-01-29 19:01:18 +0800

    修改

--- bilibili_api/splash.py
+++ bilibili_api/splash.py
@@ -1,7 +1,6 @@
 r"""
-模块：splash
-功能：客户端开屏图
-此模块作者：david082321
+模块：app
+功能：客户端API
 项目GitHub地址：https://github.com/Passkou/bilibili_api
 项目主页：https://passkou.com/bilibili_api
    _____                _____    _____   _  __   ____    _    _
@@ -16,29 +15,31 @@ from . import utils, exceptions, common
 import requests
 import re
 import json
+import time
 
 API = utils.get_api()
 
 
-def get_splash_list(mobi_app: str = "android", platform: str = "android",
+def get_loading_images(mobi_app: str = "android", platform: str = "android",
                            height: int = 1920, width: int = 1080,
                            build: int = 999999999, birth: str = "",
                            verify: utils.Verify = None):
     """
     获取开屏启动画面
     :param build: 客户端内部版本号
-    :param mobi_app: android/iphone/ipad
-    :param platform: android/ios
-    :param height: 高
-    :param width: 宽
-    :param birth: 生日日期
+    :param mobi_app: android / iphone / ipad
+    :param platform: android / ios    / ios
+    :param height: 屏幕高度
+    :param width: 屏幕宽度
+    :param birth: 生日日期(四位数，例 0101)
     :param verify:
     :return:
+    :注意：返回的图片受到许多参数影响，详细对应表请参考 docs/模块/app.md
     """
     if verify is None:
         verify = utils.Verify()
     
-    api = API["splash"]["splash"]["list"]
+    api = API["app"]["splash"]["list"]
     params = {
         "build": build,
         "mobi_app": mobi_app,
@@ -50,27 +51,28 @@ def get_splash_list(mobi_app: str = "android", platform: str = "android",
     resp = utils.get(url=api["url"], params=params, cookies=verify.get_cookies())
     return resp
 
-def get_splash_bread(mobi_app: str = "android", platform: str = "android",
+def get_loading_images_special(mobi_app: str = "android", platform: str = "android",
                            height: int = 1920, width: int = 1080,
-                           ts: int = 1609430400, verify: utils.Verify = None,
+                           ts: int = int(time.time()),
                            appkey: str = "1d8b6e7d45233436",
-                           appsec: str = "560c52ccd288fed045859ed18bffd973"):
+                           appsec: str = "560c52ccd288fed045859ed18bffd973",
+                           verify: utils.Verify = None):
     """
-    获取新版开屏启动画面
+    获取特殊开屏启动画面
     :param appkey:
     :param appsec:
-    :param mobi_app: android/iphone/ipad
-    :param platform: android/ios
-    :param height: 高
-    :param width: 宽
-    :param ts: 当前UNIX秒
+    :param mobi_app: android / iphone / ipad
+    :param platform: android / ios    / ios
+    :param height: 屏幕高度
+    :param width: 屏幕宽度
+    :param ts:
     :param verify:
     :return:
     """
     if verify is None:
         verify = utils.Verify()
 
-    api = API["splash"]["splash"]["brand"]
+    api = API["app"]["splash"]["brand"]
     sign_params = "appkey="+appkey+"&mobi_app="+mobi_app+"&platform="+platform+"&screen_height="+str(height)+"&screen_width="+str(width)+"&ts="+str(ts)+appsec
     from sys import version_info
     if(version_info.major==2):
