c54b300771458ac1d242bf71ff677a3f5f2782ff
Passkou <passkou@163.com>
2021-05-29 17:38:29 +0800

    增加直播弹幕

--- tests/test_live.py
+++ tests/test_live.py
@@ -51,3 +51,13 @@ async def test_m_unban_user():
 
 async def test_n_send_danmaku():
     return await l.send_danmaku(Danmaku(f'test_{random.randint(10000, 99999)}'))
+
+async def test_o_LiveDanmaku():
+    async def on_msg(data):
+        print(data)
+        await room.disconnect()
+
+    room = live.LiveDanmaku(22544798, True)
+    room.add_event_listener('ALL', on_msg)
+    await room.connect()
+    
