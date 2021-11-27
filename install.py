babc565579647ec7447b10d0a7360f6c5d11fcb0
MoyuScript <i@moyu.moe>
2021-11-27 20:03:34 +0800

    233

---
+++ install.py
@@ -0,0 +1,18 @@
+import os
+import shutil
+
+print("初始化开发环境中...")
+
+current_file_dir = os.path.dirname(__file__)
+
+# 初始化 Githooks
+print("初始化 GitHooks 中...")
+git_hooks_dir = os.path.join(current_file_dir, ".git/hooks")
+
+hooks_path = map(lambda x: os.path.join(current_file_dir, ".githooks", x), os.listdir(os.path.join(current_file_dir, ".githooks")))
+
+for hook in hooks_path:
+    print(f"复制 {hook} 到 {git_hooks_dir}")
+    shutil.copy(hook, git_hooks_dir)
+
+print("初始化开发环境完成")
