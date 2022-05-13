409df808cafefc26a0fb6b2af918939de697e1c1
nemo <yimoxia@Outlook.com>
2022-05-13 22:08:18 +0800

    feat!: 优化了搜索 api 入口

--- bilibili_api/misc.py
+++ bilibili_api/misc.py
@@ -1,10 +1,20 @@
 """
 杂项
 """
+from enum import Enum
 from .utils.utils import get_api
 from .utils.network import request
 
 API = get_api('misc')
+
+class SearchObjectType(Enum):
+    VIDEO = "video"
+    BANGUMI = "media_bangumi"
+    FT = "media_ft"
+    LIVE = "live"
+    ARTICLE = "article"
+    TOPIC = "topic"
+    USER = "bili_user"
 
 async def web_search(keyword: str):
     """
@@ -19,7 +29,7 @@ async def web_search(keyword: str):
     }
     return await request('GET', url=api["url"], params=params)
 
-async def web_search_by_type(keyword: str, search_type: str):
+async def web_search_by_type(keyword: str, search_type: SearchObjectType):
     """
     指定关键字和类型进行搜索，返回未经处理的字典
     类型：视频(video)、番剧(media_bangumi)、影视(media_ft)、直播(live)、专栏(article)、话题(topic)、用户(bili_user)
@@ -31,6 +41,6 @@ async def web_search_by_type(keyword: str, search_type: str):
     api = API["search"]["web_search_by_type"]
     params = {
         "keyword": keyword,
-        "search_type": search_type
+        "search_type": search_type.value
     }
     return await request('GET', url=api["url"], params=params)
--- tests/test_video_uploader.py
+++
@@ -1,5 +0,0 @@
-
-from bilibili_api import video_uploader
-
-async def test_a_get_missions():
-    return await video_uploader.get_missions()
