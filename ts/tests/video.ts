9c19359bde0d929402a99f32674ab0872e12ac43
nemo2011 <yimoxia@outlook.com>
2022-07-14 11:57:38 +0800

    fix: 测试

--- ts/tests/video.ts
+++ ts/tests/video.ts
@@ -1,10 +1,8 @@
 import { Video } from "../video";
 import { Proxy } from "../models/Proxy";
-import { set_proxy } from "../utils/network";
+import { setProxy } from "../utils/network";
 
 export function test_video() {
-    var p = new Proxy("177.136.84.134", "999");
-    set_proxy(p);
     var v = new Video(null, 2);
     v.get_info().then(function ( value) {
         console.log(value);
