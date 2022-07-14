64081bddda385dd9de7c2124ed0bb5095b9fc4ef
nemo2011 <yimoxia@outlook.com>
2022-07-14 20:55:44 +0800

    fix: 小错误修复

--- bilibili-api-js/utils/aid2bvid.ts
+++ bilibili-api-js/utils/aid2bvid.ts
@@ -9,19 +9,19 @@ var s = [11, 10, 3, 8, 4, 6],
   xor = 177451812,
   add = 8728348608;
 
-export function bvid2aid(x: any) {
+export function bvid2aid(bvid: string) {
   var r = 0;
   for (var i = 0; i < 6; i++) {
-    r += tr[x[s[i]]] * 58 ** i;
+    r += tr[bvid[s[i]]] * 58 ** i;
   }
   return ((r - add) ^ xor);
 }
 
-export function aid2bvid(x: any) {
-  x = (x ^ xor) + add;
+export function aid2bvid(aid: number) {
+  aid = (aid ^ xor) + add;
   var r = "BV1  4 1 7  ".split("");
   for (var i = 0; i < 6; i++) {
-    r[s[i]] = table[Math.floor(x / 58 ** i) % 58];
+    r[s[i]] = table[Math.floor(aid / 58 ** i) % 58];
   }
   return r.join("");
 }
