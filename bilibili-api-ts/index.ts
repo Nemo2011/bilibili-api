c60d85ff1bd72bb097b3e4fed27fa5d321e51607
Nemo2011 <yimoxia@outlook.com>
2022-07-16 15:21:33 +0800

    feat: Danmaku 类(javascript/typescript)

--- bilibili-api-ts/index.ts
+++ bilibili-api-ts/index.ts
@@ -8,4 +8,8 @@ let HEADERS = {
     "Referer": "https://www.bilibili.com"
 }
 
-export {setProxy, aid2bvid, bvid2aid, Proxy, Credential};
+function help(...config: any[]) {
+    console.log("欢迎来到 bilibili-api-ts，这个模块是 Python bilibili-api 的 Typescript 克隆，适用于 JS/TS。\n文档请前往 https://nemo2011.github.io/bilibili_api/#/README-ts 查看。")
+}
+
+export {setProxy, aid2bvid, bvid2aid, Proxy, Credential, HEADERS, help};
