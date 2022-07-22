cadcff8be8803db7d47c1994f529cc1044208ad1
--get-all <yimoxia@outlook.com>
2022-07-22 22:26:54 +0800

    docs: Update docs and readme

--- bilibili-api-ts/search.ts
+++ bilibili-api-ts/search.ts
@@ -3,6 +3,16 @@ import { request } from "./utils/network";
 
 const API = SearchData;
 
+/**
+ * 搜索对象枚举。
+    + VIDEO : 视频
+    + BANGUMI : 番剧
+    + FT : 影视
+    + LIVE : 直播
+    + ARTICLE : 专栏
+    + TOPIC : 话题
+    + USER : 用户
+ */
 export enum SearchObjectType {
     VIDEO = "video", 
     BANGUMI = "media_bangumi", 
@@ -12,7 +22,15 @@ export enum SearchObjectType {
     TOPIC = "topic", 
     USER = "bili_user"
 }
-
+/**
+ * 搜索
+ * 
+ * params keyword(string): 搜索关键词
+ * 
+ * params page(number)   : 页码
+ * 
+ * @returns {Object} 调用 API 返回的结果
+ */
 export async function web_search({keyword, page=1}: {keyword: string, page?: number}) {
     var api = API.search.web_search;
     var params = {
@@ -28,7 +46,18 @@ export async function web_search({keyword, page=1}: {keyword: string, page?: num
     );
 }
 
-export async function web_search_by_type({keyword, search_type, page=1}: {keyword: string, search_type: SearchObjectType|string, page?: number}) {
+/**
+ * 根据指定类型搜索
+ * 
+ * param keyword(string):                      搜索关键词
+ * 
+ * param search_type(SearchObjectType|string): 搜索类型
+ * 
+ * param page(number):                         页码
+ * 
+ * @returns {Object} 调用 API 返回的结果
+ */
+export async function web_search_by_type({keyword, search_type, page=1}: {keyword: string, search_type: SearchObjectType|string, page?: number}){
     var api = API.search.web_search_by_type;
     var params = {
         "keyword": keyword, 
---
+++ docs/ts/modules/search.md
@@ -0,0 +1,36 @@
+# Module search.ts(search.js)
+
+## enum SearchObjectType
+
+搜索对象枚举。
+
++ VIDEO : 视频
++ BANGUMI : 番剧
++ FT : 影视
++ LIVE : 直播
++ ARTICLE : 专栏
++ TOPIC : 话题
++ USER : 用户
+
+## _async_ function web_search()
+
+| name | type | description |
+| - | - | - |
+| keyword | string | 搜索关键词 |
+| page | number | 页码 |
+
+搜索
+
+**Returns:** Object: 调用 API 返回的结果
+
+## _async_ function web_search_by_type()
+
+| name | type | description |
+| - | - | - |
+| keyword | string | 搜索关键词 |
+| search_type | SearchObjectType\|string | 搜索类型 | 
+| page | number | 页码 |
+
+根据指定类型搜索
+
+**Returns:** Object: 调用 API 返回的结果
