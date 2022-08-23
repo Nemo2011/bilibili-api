b7c11d731c64c4472c6240cd22482e8205be684e
--get-all <yimoxia@outlook.com>
2022-08-23 14:06:11 +0800

    feat: bilidown 普通视频完成

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -12,23 +12,73 @@ from .utils.sync import sync
 from .utils.network_httpx import get_session, set_session
 from .utils.parse_link import parse_link, ResourceType
 from . import (
-    app, article, ass, audio, 
-    bangumi, 
-    channel, cheese, comment, 
-    dynamic, 
-    favorite_list, 
-    homepage, 
-    interactive_video, 
-    live, login_func, login, 
-    rank, 
-    search, settings, 
-    user, 
-    video_uploader, video
+    app,
+    article,
+    ass,
+    audio,
+    bangumi,
+    channel,
+    cheese,
+    comment,
+    dynamic,
+    favorite_list,
+    homepage,
+    interactive_video,
+    live,
+    login_func,
+    login,
+    rank,
+    search,
+    settings,
+    user,
+    video_uploader,
+    video,
 )
 
 # UA 头 + Referer
-HEADERS = {"User-Agent": "Mozilla/5.0", "Referer": "https://bilibili.com"}
+HEADERS = {"User-Agent": "Mozilla/5.0", "Referer": "https://www.bilibili.com"}
 
 # 如果系统为 Windows，则修改默认策略，以解决代理报错问题
 if "windows" in platform.system().lower():
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+__all__ = [
+    "Credential",
+    "Danmaku",
+    "DmFontSize",
+    "DmMode",
+    "HEADERS",
+    "ResourceType",
+    "aid2bvid",
+    "app",
+    "article",
+    "ass",
+    "asyncio",
+    "audio",
+    "bangumi",
+    "bvid2aid",
+    "channel",
+    "cheese",
+    "comment",
+    "dynamic",
+    "exceptions",
+    "favorite_list",
+    "get_real_url",
+    "get_session",
+    "homepage",
+    "interactive_video",
+    "live",
+    "login",
+    "login_func",
+    "parse_link",
+    "platform",
+    "rank",
+    "search",
+    "set_session",
+    "settings",
+    "sync",
+    "user",
+    "utils",
+    "video",
+    "video_uploader",
+]
--- bilidown/__init__.py
+++ bilidown/__init__.py
@@ -1,2 +1,4 @@
 # BiliDown
+# 哔哩哔哩命令行下载器
 # Powered by Bilibili API
+__author__ = "Nemo2011"
