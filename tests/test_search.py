fa6be03f1d17f46f8ab7e0239af575cf67ef2e4b
sudoskys <coldlando@hotmail.com>
2022-09-16 11:45:01 +0800

    实例和测试

--- tests/test_search.py
+++ tests/test_search.py
@@ -21,3 +21,14 @@ async def test_d_get_default_search_keyword():
 
 async def test_e_get_suggest_keywords():
     return await search.get_suggest_keywords("gswdm")
+
+
+async def test_f_search_by_order():
+    return await search.search_by_type("小马宝莉", search_type=search.SearchObjectType.USER,
+                                       rank_order=search.UserOrder.LEVEL)
+
+
+from bilibili_api import sync
+
+res = sync(test_f_search_by_order())
+print(res)
