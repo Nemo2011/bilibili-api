d0b513e0cfdd91aa3d37e7ed18a25a67376d80c5
--get-all <yimoxia@outlook.com>
2022-10-19 18:34:25 +0800

    docs(game): 注释

--- bilibili_api/game.py
+++ bilibili_api/game.py
@@ -5,8 +5,20 @@ from .utils.utils import get_api
 API = get_api("game")
 
 class Game:
-    def __init__(self, game_id: str):
+    def __init__(self, game_id: int, credential: Credential = None):
+        """
+        Args:
+            game_id(int)          : 游戏 id
+            credential(Credential): 凭据类
+        """
         self.__game_id = game_id
+        self.credential = credential if credential else Credential()
     
     def get_game_id(self):
+        """
+        获取游戏 id
+
+        Returns:
+            游戏 id
+        """
         return self.__game_id
