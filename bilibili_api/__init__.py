4216a1db79c980c14230c7fb69c4402267dd2b7c
Nemo2011 <yimoxia@outlook.com>
2022-12-18 08:40:27 +0800

    fix: __init__.py

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -18,26 +18,9 @@ from .utils.get_item import GetItemObjectType, get_item
 from .utils.parse_link import ResourceType, parse_link
 from .utils.short import get_real_url
 from .utils.sync import sync
-from .utils.AsyncEvent import AsyncEvent
+from .utils.network_httpx import get_session, set_session
 
 # 如果系统为 Windows，则修改默认策略，以解决代理报错问题
 if "windows" in platform.system().lower():
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 del asyncio, platform
-
-__all__ = ['ApiException', 'ArgsException', 'AsyncEvent', 'Credentia\
-    l', 'CredentialNoBiliJctException', 'CredentialNoBuvid3Exception\
-    ', 'CredentialNoDedeUserIDException', 'CredentialNoSessdataExcep\
-    tion', 'Danmaku', 'DanmakuClosedException', 'DmFontSize', 'DmMod\
-    e', 'DynamicExceedImagesException', 'GetItemObjectType', 'HEADER\
-    S', 'LiveException', 'LoginError', 'NetworkException', 'Resource\
-    Type', 'ResponseCodeException', 'ResponseException', 'SpecialDan\
-    maku', 'VideoUploadException', '__builtins__', '__cached__', '__\
-    doc__', '__file__', '__loader__', '__name__', '__package__', '__\
-    path__', '__spec__', 'aid2bvid', 'app', 'article', 'ass', 'audio\
-    ', 'bangumi', 'black_room', 'bvid2aid', 'channel', 'cheese', 'co\
-    mment', 'dynamic', 'errors', 'exceptions', 'favorite_list', 'gam\
-    e', 'get_item', 'get_real_url', 'homepage', 'interactive_video',\
-     'live', 'login', 'login_func', 'parse_link', 'rank', 'search', \
-    'settings', 'sync', 'topic', 'user', 'utils', 'video', 'video_up\
-    loader']
