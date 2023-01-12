39755e068675b7096ac3988b825d63e91d49288b
Nemo2011 <yimoxia@outlook.com>
2023-01-12 10:05:00 +0800

    tests: 取消密码登录测试

--- tests/test_login.py
+++ tests/test_login.py
@@ -7,5 +7,5 @@ username = os.getenv("BILI_PHONE")
 password = os.getenv("BILI_PASSWORD")
 
 
-async def test_a_password_login():
-    return login.login_with_password(username, password)
+# async def test_a_password_login():
+#     return login.login_with_password(username, password)
