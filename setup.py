cd1c7e837e90175474b24b6d707843109ec81048
Nemo2011 <yimoxia@outlook.com>
2023-01-16 10:48:02 +0800

    chore: 新增漫画相关接口信息

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf8") as f:
 
 setuptools.setup(
     name="bilibili-api-python",
-    version="14.0.0",
+    version="14.1.0.dev",
     license="GPLv3+",
     author="Nemo2011",
     author_email="yimoxia@outlook.com",
