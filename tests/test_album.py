502907e8b5f5f6eb773965f8241cf18941f7b749
Nemo2011 <yimoxia@outlook.com>
2023-01-19 13:55:55 +0800

    tests: 添加相簿的测试

---
+++ tests/test_album.py
@@ -0,0 +1,20 @@
+# bilibili_api.album
+
+from bilibili_api import album
+
+al = album.Album(123348276)
+
+async def test_a_Album_get_info():
+    return await al.get_info()
+
+async def test_b_Album_get_pictures():
+    return await al.get_pictures()
+
+async def test_c_get_homepage_albums():
+    return await album.get_homepage_albums_list()
+
+async def test_d_get_recommend_uppers():
+    return await album.get_homepage_recommend_uppers()
+
+async def test_e_get_user_albums():
+    return await album.get_user_albums(53456)
