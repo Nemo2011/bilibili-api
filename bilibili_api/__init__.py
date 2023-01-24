5b15561223336587d2f5691999ba8c0927234d79
Nemo2011 <yimoxia@outlook.com>
2023-01-24 21:14:57 +0800

    chore: Update project meta 将 v14.2.0 改为 v15.0.0.dev

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -6,66 +6,30 @@ bilibili_api
 
 import asyncio
 import platform
+import os
 
-from . import (
-    album,
-    app,
-    article,
-    ass,
-    audio,
-    bangumi,
-    black_room,
-    channel,
-    cheese,
-    client,
-    comment,
-    dynamic,
-    emoji,
-    favorite_list,
-    game,
-    homepage,
-    interactive_video,
-    live_area,
-    live,
-    login_func,
-    login,
-    manga,
-    note,
-    rank,
-    search,
-    session,
-    settings,
-    topic,
-    user,
-    video_uploader,
-    video,
-    vote,
-)
-
-from .errors import (
-    ApiException,
-    ResponseCodeException,
-    ResponseException,
-    NetworkException,
-    ArgsException,
-    CredentialNoSessdataException,
-    CredentialNoBiliJctException,
-    CredentialNoBuvid3Exception,
-    CredentialNoDedeUserIDException,
-    DanmakuClosedException,
-    VideoUploadException,
-    LoginError,
-    LiveException,
-    DynamicExceedImagesException,
-)
+from . import (album, app, article, ass, audio, bangumi, black_room, channel,
+               cheese, client, comment, dynamic, emoji, favorite_list, game,
+               homepage, interactive_video, live, live_area, login, login_func,
+               manga, note, rank, search, session, settings, topic, user,
+               video, video_uploader, vote)
+from .errors import (ApiException, ArgsException, CredentialNoBiliJctException,
+                     CredentialNoBuvid3Exception,
+                     CredentialNoDedeUserIDException,
+                     CredentialNoSessdataException, DanmakuClosedException,
+                     DynamicExceedImagesException, LiveException, LoginError,
+                     NetworkException, ResponseCodeException,
+                     ResponseException, VideoUploadException)
 from .utils.aid_bvid_transformer import aid2bvid, bvid2aid
 from .utils.Credential import Credential
 from .utils.Danmaku import Danmaku, DmFontSize, DmMode, SpecialDanmaku
-from .utils.Picture import Picture
+from .utils.network_httpx import HEADERS, get_session, set_session
 from .utils.parse_link import ResourceType, parse_link
+from .utils.Picture import Picture
 from .utils.short import get_real_url
 from .utils.sync import sync
-from .utils.network_httpx import get_session, set_session, HEADERS
+
+BILIBILI_API_VERSION = "15.0.0.dev"
 
 # 如果系统为 Windows，则修改默认策略，以解决代理报错问题
 if "windows" in platform.system().lower():
@@ -103,4 +67,5 @@ __all__ = [
     "rank", "search", "session", "set_session",
     "settings", "sync", "topic", "user", "video",
     "video_uploader", "vote",
+    "BILIBILI_API_VERSION"
 ]
