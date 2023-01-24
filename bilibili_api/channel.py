6365725c7106140b369942cc096b70cc6c855656
Nemo2011 <yimoxia@outlook.com>
2023-01-24 16:54:36 +0800

    feat: Channel 类新增获取视频的接口

--- bilibili_api/channel.py
+++ bilibili_api/channel.py
@@ -11,7 +11,7 @@ from bilibili_api.errors import ResponseException, ApiException
 import re
 import json
 from enum import Enum
-from typing import Optional, List
+from typing import Optional, List, Union
 
 API = get_api("channel")
 
@@ -58,11 +58,11 @@ async def get_channel_categories() -> dict:
 
 async def get_channel_category_detail(category_id: int, offset: str = "0", credential: Optional[Credential] = None) -> dict:
     """
-    获取频道分类的详细信息 (包括分类中的所有频道，每个频道的信息)
+    获取频道分类的频道列表及其详细信息
 
     Args:
         category_id (int): 频道分类的 id。
-        offset      (str): 偏移值（下一页的第一个动态 ID，为该请求结果中的 offset 键对应的值），类似单向链表. Defaults to "0"
+        offset      (str): 偏移值（下面的数据的第一个频道 ID，为该请求结果中的 offset 键对应的值），类似单向链表. Defaults to "0"
     """
     credential = credential if credential else Credential()
     api = API["categories"]["sub_channels"]
@@ -76,6 +76,42 @@ async def get_channel_category_detail(category_id: int, offset: str = "0", crede
         params=params,
         credential=credential
     )
+
+
+class ChannelVideosOrder(Enum):
+    """
+    频道视频排序方式。
+
+    - NEW: 最新
+    - HOT: 最火
+    - VIEW: 播放量最高
+    """
+    NEW = "?sort_type=new"
+    HOT = "?sort_type=hot"
+    VIEW = "?sort_type=view"
+
+
+class ChannelVideosFilter(Enum):
+    """
+    频道视频筛选条件
+
+    - ALL     : 全部
+    - YEAR_年份: 指定年份筛选
+    """
+    ALL = "?filter_type="
+    YEAR_2023 = "?filter_type=2023"
+    YEAR_2022 = "?filter_type=2022"
+    YEAR_2021 = "?filter_type=2021"
+    YEAR_2020 = "?filter_type=2020"
+    YEAR_2019 = "?filter_type=2019"
+    YEAR_2018 = "?filter_type=2018"
+    YEAR_2017 = "?filter_type=2017"
+    YEAR_2016 = "?filter_type=2016"
+    YEAR_2015 = "?filter_type=2015"
+    YEAR_2014 = "?filter_type=2014"
+    YEAR_2013 = "?filter_type=2013"
+    YEAR_2012 = "?filter_type=2012"
+    YEAR_2011 = "?filter_type=2011"
 
 
 class Channel:
@@ -111,6 +147,36 @@ class Channel:
             dict: HTML 中 window.__INITIAL_STATE__ 中的信息
         """
         return (await self.get_info())["channelDetailBanner"]["data"]["tag_channels"]
+
+    async def get_list(
+        self,
+        order_or_filter: Union[ChannelVideosOrder, ChannelVideosFilter, None] = None,
+        offset: str = "",
+        page_size: int = 30
+    ) -> dict:
+        """
+        获取频道的所有视频
+
+        Args:
+            order_or_filter (ChannelVideosOrder | ChannelVideosFilter | None): 获取视频的相关选项
+            offset          (str)                                            : 偏移值（下面的第一个视频的 ID，为该请求结果中的 offset 键对应的值），类似单向链表. Defaults to "0"
+            page_size       (int)                                            : 每页的数据大小
+
+        Returns:
+            dict: 调用 API 返回的结果
+        """
+        api = API["channel"]["list_filter"]
+        if isinstance(order_or_filter, ChannelVideosOrder):
+            api = API["channel"]["list_multiple"]
+        if isinstance(order_or_filter, Enum):
+            api["url"] += order_or_filter.value
+        params = {
+            "channel_id": self.get_channel_id()
+        }
+        return await request(
+            "GET", api["url"], params=params
+        )
+
 
 
 async def get_channels_in_category(category_id: int) -> List["Channel"]:
