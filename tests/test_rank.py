d56ec0b492a446defb0086a89a624c57992dd8f8
z0z0r4 <z0z0r4@outlook.com>
2023-02-02 14:19:40 +0800

    test: test_a_get_rank

--- tests/test_rank.py
+++ tests/test_rank.py
@@ -1,10 +1,39 @@
 # bilibili_api.rank
 
 from bilibili_api import rank
+from bilibili_api.rank import RankType, RankDayType
 
 
 async def test_a_get_rank():
-    return await rank.get_rank()
+    rank_types = [RankType.All,
+                        RankType.Original,
+                        RankType.Rookie,
+                        RankType.Bangumi,
+                        RankType.GuochuanAnime,
+                        RankType.Guochuang,
+                        RankType.Documentary,
+                        RankType.Douga,
+                        RankType.Music,
+                        RankType.Dance,
+                        RankType.Game,
+                        RankType.Knowledge,
+                        RankType.Technology,
+                        RankType.Sports,
+                        RankType.Car,
+                        RankType.Life,
+                        RankType.Food,
+                        RankType.Animal,
+                        RankType.Kitchen,
+                        RankType.Fashion,
+                        RankType.Ent,
+                        RankType.Cinephile,
+                        RankType.Movie,
+                        RankType.TV,
+                        RankType.Variety,
+                        RankType.Original
+                    ]
+    for rank_type in rank_types:
+        await rank.get_rank(rank_type, RankDayType.WEEK)
 
 
 async def test_b_get_hot_video():
