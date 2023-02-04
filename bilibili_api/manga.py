4c33a72a825b5f2e3b2be6bfb8a47bc65b6e0aad
z0z0r4 <z0z0r4@outlook.com>
2023-02-04 15:47:28 +0800

    feat: manga.get_index

--- bilibili_api/manga.py
+++ bilibili_api/manga.py
@@ -12,6 +12,7 @@ from bilibili_api.utils.Picture import Picture
 from typing import Optional, List, Dict, Union
 from urllib.parse import urlparse
 import httpx
+from enum import Enum
 
 API = get_api("manga")
 
@@ -184,7 +185,7 @@ async def manga_image_url_turn_to_Picture(url: str, credential: Optional[Credent
             "POST", token_api["url"], data=datas,
             no_csrf=(False if (credential.has_sessdata() and credential.has_bili_jct()) else True)
         )
-        return token_data[0]["url"] + "?token=" + token_data[0]["token"]
+        return f'{token_data[0]["url"]}?token={token_data[0]["token"]}'
     url = await get_real_image_url(url)
     return Picture.from_url(url)
 
@@ -212,3 +213,146 @@ async def set_follow_manga(manga: Manga, status: bool = True, credential: Option
         "comic_ids": str(manga.get_manga_id())
     }
     return await request("POST", api["url"], data=data, credential=credential)
+
+class MangaIndexFilter:
+    """
+    漫画索引筛选器类。
+    """
+
+    class Area(Enum):
+        """
+        漫画索引筛选器的地区枚举类。
+
+        - ALL: 全部
+        - CHINA: 大陆
+        - JAPAN: 日本
+        - SOUTHKOREA: 韩国
+        - OTHER: 其他
+        """
+        ALL = -1
+        CHINA = 1
+        JAPAN = 2
+        SOUTHKOREA = 6
+        OTHER = 5
+
+    class Order(Enum):
+        """
+        漫画索引筛选器的排序枚举类。
+
+        - HOT: 人气推荐
+        - UPDATE: 更新时间
+        - RELEASE_DATE: 上架时间
+        """
+        HOT = 0
+        UPDATE = 1
+        RELEASE_DATE = 3
+    
+    class Status(Enum):
+        """
+        漫画索引筛选器的状态枚举类。
+
+        - ALL: 全部
+        - FINISHED: 完结
+        - UNFINISHED: 连载
+        """
+        ALL = -1
+        FINISHED = 1
+        UNFINISHED = 0
+
+    class Payment(Enum):
+        """
+        漫画索引筛选器的付费枚举类。
+
+        - ALL: 全部
+        - FREE: 免费
+        - PAID: 付费
+        - WILL_BE_FREE: 等就免费
+        """
+        ALL = -1
+        FREE = 1
+        PAID = 2
+        WILL_BE_FREE = 3
+
+    class Style(Enum):
+        """
+        漫画索引筛选器的风格枚举类。
+
+        - ALL: 全部
+        - WARM: 热血
+        - ANCIENT: 古风
+        - FANTASY: 玄幻
+        - IMAGING: 奇幻
+        - SUSPENSE: 悬疑
+        - CITY: 都市
+        - HISTORY: 历史
+        - WUXIA: 武侠仙侠
+        - GAME: 游戏竞技
+        - PARANORMAL: 悬疑灵异
+        - ALTERNATE: 架空
+        - YOUTH: 青春
+        - WEST_MAGIC: 西幻
+        - MORDEN: 现代
+        - POSITIVE: 正能量
+        - SCIENCE_FICTION: 科幻
+        """
+        ALL = -1
+        WARM = 999
+        ANCIENT = 997
+        FANTASY = 1016
+        IMAGING = 998
+        SUSPENSE = 1023
+        CITY = 1002
+        HISTORY = 1096
+        WUXIA = 1092
+        GAME = 1088
+        PARANORMAL = 1081
+        ALTERNATE = 1063
+        YOUTH = 1060
+        WEST_MAGIC = 1054
+        MORDEN = 1048
+        POSITIVE = 1028
+        SCIENCE_FICTION = 1027
+
+async def get_raw_index(area: MangaIndexFilter.Area = MangaIndexFilter.Area.ALL,
+                            order: MangaIndexFilter.Order = MangaIndexFilter.Order.HOT,
+                            status: MangaIndexFilter.Status = MangaIndexFilter.Status.ALL,
+                            payment: MangaIndexFilter.Payment = MangaIndexFilter.Payment.ALL,
+                            style: MangaIndexFilter.Style = MangaIndexFilter.Style.ALL,
+                            pn: int = 1,
+                            ps: int = 18,
+) -> list:
+    """
+    获取漫画索引
+
+    Returns:
+        dict: 调用 API 返回的结果
+    """
+    api = API["info"]["index"]
+    params = {"device": "pc", "platform": "web"}
+    data = {
+        "area_id": area.value,
+        "order": order.value,
+        "is_finish": status.value,
+        "is_free": payment.value,
+        "style_id": style.value,
+        "page_num": pn,
+        "page_size": ps
+    }
+    return await request("POST", api["url"], data=data, params=params, no_csrf=True)
+
+async def get_index(area: MangaIndexFilter.Area = MangaIndexFilter.Area.ALL,
+                            order: MangaIndexFilter.Order = MangaIndexFilter.Order.HOT,
+                            status: MangaIndexFilter.Status = MangaIndexFilter.Status.ALL,
+                            payment: MangaIndexFilter.Payment = MangaIndexFilter.Payment.ALL,
+                            style: MangaIndexFilter.Style = MangaIndexFilter.Style.ALL,
+                            pn: int = 1,
+                            ps: int = 18,
+) -> List[Manga]:
+    """
+    获取漫画索引
+
+    Returns:
+        List[Manga]: 漫画索引
+    """
+    data = await get_raw_index(area, order, status, payment, style, pn, ps)
+    return [Manga(manga_data["season_id"]) for manga_data in data]
