32a7efad73aa839cd5d3cfb23de3a3ab1cf9beaf
z0z0r4 <z0z0r4@outlook.com>
2023-02-12 08:59:38 +0800

    test: creative_center

---
+++ tests/test_creative_center.py
@@ -0,0 +1,28 @@
+from bilibili_api import creative_center
+from . import common
+
+credential = common.get_credential()
+
+async def test_a_get_compare():
+    return await creative_center.get_compare(credential)
+
+async def test_b_get_graph():
+    return await creative_center.get_graph(credential)
+
+async def test_c_get_overview():
+    return await creative_center.get_overview(credential)
+
+async def test_d_get_video_survey():
+    return await creative_center.get_video_survey(credential)
+
+async def test_e_get_video_playanalysis():
+    return await creative_center.get_video_playanalysis(credential)
+
+async def test_f_get_video_source():
+    return await creative_center.get_video_source(credential)
+
+async def test_g_get_fan_overview():
+    return await creative_center.get_fan_overview(credential)
+
+async def test_h_get_fan_graph():
+    return await creative_center.get_fan_graph(credential)
