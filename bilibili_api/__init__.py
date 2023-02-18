96f9b37908b8ec275456591239953487991b45d9
Nemo2011 <yimoxia@outlook.com>
2023-02-18 15:36:45 +0800

    release: v15.1.0
    # **15.1.0** 2023/02/18
    
    - 新增 `creative_center` 模块 (#194)
    - 新增 `manga.get_manga_home_recommend` 函数用于查看漫画首页推荐 (#192)
    - 新增 `manga.get_manga_update` 函数用于查看更新推荐的漫画 (#192)
    - 新增 `video.get_cid_info` 函数用于查看 `cid` 具体信息 (采用 <https://hd.biliplus.com> 的 API)
    - 修复验证码登录时需要验证时不返回 `Check` 类的问题
    - 修复视频上传无法使用上传的封面的问题 (#200)
    - 优化 `interactive_video.InteractiveVideoDownloader` 性能 & 增加三种下载模式

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -29,7 +29,7 @@ from .utils.Picture import Picture
 from .utils.short import get_real_url
 from .utils.sync import sync
 
-BILIBILI_API_VERSION = "15.0.0"
+BILIBILI_API_VERSION = "15.1.0"
 
 # 如果系统为 Windows，则修改默认策略，以解决代理报错问题
 if "windows" in platform.system().lower():
