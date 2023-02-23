51a15c05cf7dcabeac0a6bcdfef6b8af1d9269da
z0z0r4 <z0z0r4@outlook.com>
2023-02-23 22:46:12 +0800

    feat: class Vote

--- bilibili_api/vote.py
+++ bilibili_api/vote.py
@@ -23,20 +23,6 @@ class VoteType(Enum):
     TEXT = 0
     IMAGE = 1
 
-async def get_vote_info(vote_id: int) -> dict:
-    """
-    获取投票详情
-
-    Args:
-        vote_id (int): vote_id, 获取：https://nemo2011.github.io/bilibili-api/#/vote_id
-
-    Returns:
-        dict: 调用 API 返回的结果
-    """
-    api = API["info"]
-    params = {"vote_id": vote_id}
-    return await request("GET", api["url"], params=params)
-
 class VoteChoices:
     """
     投票选项类
@@ -79,6 +65,74 @@ class VoteChoices:
             results[f'{choice_key_name}[img_url]'] = self.choices[i]["img_url"]
         return results
 
+class Vote:
+    """
+    投票类
+
+    Attributes:
+        vote_id (int): vote_id, 获取：https://nemo2011.github.io/bilibili-api/#/vote_id
+        credential (Credential): 凭据类
+    """
+    def __init__(self, vote_id: int, credential: Credential = Credential()) -> None:
+        """
+        Args:
+            vote_id (int): vote_id, 获取：https://nemo2011.github.io/bilibili-api/#/vote_id
+            credential (Credential): 凭据类.
+        """
+        self.vote_id = vote_id
+        self.credential = credential
+
+    async def get_vote_info(self) -> dict:
+        """
+        获取投票详情
+
+        Returns:
+            dict: 调用 API 返回的结果
+        """
+        api = API["info"]
+        params = {"vote_id": self.vote_id}
+        return await request("GET", api["url"], params=params)
+
+    async def update_vote(self,
+        title: str,
+        _type: VoteType,
+        choice_cnt: int,
+        duration: int,
+        choices: VoteChoices,
+        desc: Optional[str] = None
+    ) -> dict:
+        """
+        更新投票内容
+
+        Args:
+            vote_id (int): vote_id
+            title (str): 投票标题
+            _type (VoteType): 投票类型
+            choice_cnt (int): 最多几项
+            duration (int): 投票持续秒数 常用: 三天:259200 七天:604800 三十天:2592000
+            choices (VoteChoices): 投票选项
+            credential (Credential): Credential
+            desc (Optional[str], optional): 投票描述. Defaults to None.
+
+        Returns:
+            dict: 调用 API 返回的结果
+        """
+        self.credential.raise_for_no_sessdata()
+        api = API["create"]
+        data = {
+            "info[title]": title,
+            "info[desc]": desc,
+            "info[type]": _type.value,
+            "info[choice_cnt]": choice_cnt,
+            "info[duration]": duration,
+            "info[vote_id]": self.vote_id
+        }
+        data.update(choices.get_choices())
+        if choice_cnt > len(choices.choices):
+            raise ValueError("choice_cnt 大于 choices 选项数")
+        return await request("POST", api["url"], data=data, credential=self.credential)
+
+
 async def create_vote(
     title: str,
     _type: VoteType,
@@ -87,7 +141,7 @@ async def create_vote(
     choices: VoteChoices,
     credential: Credential,
     desc: Optional[str] = None
-) -> dict:
+) -> Vote:
     """
     创建投票
 
@@ -101,7 +155,7 @@ async def create_vote(
         desc (Optional[str], optional): 投票描述. Defaults to None.
 
     Returns:
-        dict: 调用 API 返回的结果
+        Vote: Vote 类
     """
     api = API["create"]
     data = {"info[title]": title,
@@ -113,4 +167,5 @@ async def create_vote(
     data.update(choices.get_choices())
     if choice_cnt > len(choices.choices):
         raise ValueError("choice_cnt 大于 choices 选项数")
-    return await request("POST", api["url"], data=data, credential=credential)
+    vote_id = (await request("POST", api["url"], data=data, credential=credential))["vote_id"]
+    return Vote(vote_id=vote_id, credential=credential)
