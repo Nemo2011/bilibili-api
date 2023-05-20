129af14d68e2a4a4bbff23b9d56d4940006c4673
z0z0r4 <z0z0r4@outlook.com>
2023-05-20 21:32:05 +0800

    v15.4.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", "r", encoding="utf8") as f:
 
 setuptools.setup(
     name="bilibili-api-python",
-    version="15.4.2",
+    version="15.4.3",
     license="GPLv3+",
     author="Nemo2011",
     author_email="yimoxia@outlook.com",
