0fbba1e28e09b13a84c620d1627fc5e137d13703
z0z0r4 <z0z0r4@outlook.com>
2023-06-18 16:58:22 +0800

    release: v15.5.0

--- bilibili_api/__init__.py
+++ bilibili_api/__init__.py
@@ -72,7 +72,7 @@ from .utils.picture import Picture
 from .utils.short import get_real_url
 from .utils.sync import sync
 
-BILIBILI_API_VERSION = "15.4.4"
+BILIBILI_API_VERSION = "15.5.0"
 
 # 如果系统为 Windows，则修改默认策略，以解决代理报错问题
 if "windows" in platform.system().lower():
