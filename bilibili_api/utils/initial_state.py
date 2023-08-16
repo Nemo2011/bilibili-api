09d6a1e57bb6a9d37e598e20f6102feb168594a9
z0z0r4 <z0z0r4@outlook.com>
2023-08-16 23:49:05 +0800

    fix: bangumi.Episode init info

--- bilibili_api/utils/initial_state.py
+++ bilibili_api/utils/initial_state.py
@@ -5,8 +5,9 @@ bilibili_api.utils.initial_state
 """
 import re
 import json
-
 import httpx
+from enum import Enum
+from typing import Union
 
 from ..exceptions import *
 from .short import get_real_url
@@ -14,7 +15,15 @@ from .credential import Credential
 from .network_httpx import get_session
 
 
-async def get_initial_state(url: str, credential: Credential = Credential()) -> dict:
+class InitialDataType(Enum):
+    '''
+    识别返回类型
+    '''
+    INITIAL_STATE = "window.__INITIAL_STATE__"
+    NEXT_DATA = "__NEXT_DATA__"
+
+
+async def get_initial_state(url: str, credential: Credential = Credential()) -> Union[dict, InitialDataType]:
     '''
     异步获取初始化信息
 
@@ -38,15 +47,22 @@ async def get_initial_state(url: str, credential: Credential = Credential()) -> 
         pattern = re.compile(r"window.__INITIAL_STATE__=(\{.*?\});")
         match = re.search(pattern, content)
         if match is None:
-            raise ApiException("未找到相关信息")
+            pattern = re.compile(pattern = r'<script id="__NEXT_DATA__" type="application/json">\s*(.*?)\s*</script>')
+            match = re.search(pattern, content)
+            content_type = InitialDataType.NEXT_DATA
+            if match is None:
+                raise ApiException("未找到相关信息")
+        else:
+            content_type = InitialDataType.INITIAL_STATE
         try:
             content = json.loads(match.group(1))
         except json.JSONDecodeError:
             raise ApiException("信息解析错误")
 
-        return content
+        return content, content_type
 
-def get_initial_state_sync(url: str, credential: Credential = Credential()) -> dict:
+
+def get_initial_state_sync(url: str, credential: Credential = Credential()) -> Union[dict, InitialDataType]:
     '''
     同步获取初始化信息
 
@@ -69,41 +85,16 @@ def get_initial_state_sync(url: str, credential: Credential = Credential()) -> d
         pattern = re.compile(r"window.__INITIAL_STATE__=(\{.*?\});")
         match = re.search(pattern, content)
         if match is None:
-            raise ApiException("未找到相关信息")
+            pattern = re.compile(pattern = r'<script id="__NEXT_DATA__" type="application/json">\s*(.*?)\s*</script>')
+            match = re.search(pattern, content)
+            content_type = InitialDataType.NEXT_DATA
+            if match is None:
+                raise ApiException("未找到相关信息")
+        else:
+            content_type = InitialDataType.INITIAL_STATE
         try:
             content = json.loads(match.group(1))
         except json.JSONDecodeError:
             raise ApiException("信息解析错误")
 
-        return content
-    
-def get_next_data(url: str, credential: Credential = Credential()) -> dict:
-    '''
-    同步获取剧集的数据
-
-    Args:
-        url (str): 链接
-
-        credential (Credential, optional): 用户凭证. Defaults to Credential().
-    '''
-    try:
-        resp = httpx.get(
-            url,
-            cookies=credential.get_cookies(),
-            headers={"User-Agent": "Mozilla/5.0"},
-            follow_redirects=True,
-        )
-    except Exception as e:
-        raise e
-    else:
-        content = resp.text
-        pattern = re.compile(pattern = r'<script id="__NEXT_DATA__" type="application/json">\s*(.*?)\s*</script>')
-        match = re.search(pattern, content)
-        if match is None:
-            raise ApiException("未找到相关信息")
-        try:
-            content = json.loads(match.group(1))
-        except json.JSONDecodeError:
-            raise ApiException("信息解析错误")
-
-        return content
+        return content, content_type
