098f1eb3268ae9678d1c603e71df6d90a6f59bc1
littleblackLB <q2365835320@163.com>
2023-09-06 20:39:44 +0800

    test: test_get_user_info, test_create_order

---
+++ tests/test_show.py
@@ -0,0 +1,23 @@
+# bilibili_api.show
+
+from bilibili_api import Credential
+from show import BuyerInfo, Session, Ticket, OrderTicket, get_available_sessions, get_all_buyer_info
+
+
+async def test_get_user_info(project_id):
+    cre = Credential(sessdata="<SESSDATA>")
+    buyer: BuyerInfo = (await get_all_buyer_info(cre, True))[0]
+    session: Session = (await get_available_sessions(project_id))[0]
+    ticket: Ticket = session.ticket_list[0]
+    ot = OrderTicket(credential=cre, buyer_info=buyer, project_id=project_id, session=session, ticket=ticket)
+    print(await ot.get_token())
+
+
+async def test_create_order(project_id):
+    cre = Credential(sessdata="<SESSDATA>")
+    buyer: BuyerInfo = (await get_all_buyer_info(cre, True))[0]
+    session: Session = (await get_available_sessions(project_id))[0]
+    ticket: Ticket = session.ticket_list[0]
+    ot = OrderTicket(credential=cre, buyer_info=buyer, project_id=project_id, session=session, ticket=ticket)
+    res = await ot.create_order()
+    print(res)
