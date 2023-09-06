a23744849274dbc706d78da71a1e5ac8895f7bd9
Drelf2018 <drelf2018@outlook.com>
2023-09-07 00:01:34 +0800

    fix: 优化 show.py 结构

--- bilibili_api/show.py
+++ bilibili_api/show.py
@@ -3,9 +3,10 @@ bilibili_api.show
 
 展出相关
 """
+import json
 import time
 from dataclasses import dataclass, field
-from typing import List, Union
+from typing import List
 
 from .utils.credential import Credential
 from .utils.network import Api
@@ -20,12 +21,15 @@ class Ticket:
     票对象
 
     id (int): 场次id
+
     price (float): 价格(RMB)
+
     desc (str): 描述
+
     sale_start (str): 开售开始时间
+
     sale_end (str): 开售结束时间
     """
-
     id: int
     price: int
     desc: str
@@ -39,11 +43,13 @@ class Session:
     场次对象
 
     id (int): 场次id
+
     start_time (int): 场次开始时间戳
-    formatted_time (int): 格式化start_time后的时间格式: YYYY-MM-DD dddd
+
+    formatted_time (str): 格式化start_time后的时间格式: YYYY-MM-DD dddd
+
     ticket_list (list[Ticket]): 存放Ticket对象的list
     """
-
     id: int
     start_time: int
     formatted_time: str
@@ -55,18 +61,51 @@ class BuyerInfo:
     """
     购买人信息
 
-    personal_id (str): 身份证ID
-    name (str): 姓名
-    tel (str): 电话号码
-    is_fault (bool): 是否为默认
-    org (str): 原始数据
-    """
+    id (int): 信息序号
 
+    uid (int): 用户 ID
+
+    account_channel (str): 默认为空
+
+    personal_id (str): 身份证号
+
+    name (str): 姓名
+
+    id_card_front (str): 未知
+
+    id_card_back (str): 未知
+
+    is_default (bool): 是否为默认信息
+
+    tel (str): 电话号码
+
+    error_code (str): 错误代码
+
+    id_type (int): 默认 0
+
+    verify_status (int): 认证状态
+
+    accountId (int): 用户 ID
+
+    isBuyerInfoVerified (bool): 默认为 True
+
+    isBuyerValid (bool): 默认为 True
+    """
+    id: int
+    uid: int
+    account_channel: str
     personal_id: str
     name: str
+    id_card_front: str
+    id_card_back: str
+    is_default: int
     tel: str
-    is_default: bool
-    org: str
+    error_code: str
+    id_type: int
+    verify_status: int
+    accountId: int
+    isBuyerInfoVerified: bool = True
+    isBuyerValid: bool = True
 
 
 async def get_project_info(project_id: int) -> dict:
@@ -80,11 +119,10 @@ async def get_project_info(project_id: int) -> dict:
         dict: 调用 API 返回的结果
     """
     api = API["info"]["get"]
-    params = {"id": project_id}
-    return await Api(**api, ignore_code=True).update_params(**params).result
+    return await Api(**api).update_params(id=project_id).result
 
 
-async def get_available_sessions(project_id: int):
+async def get_available_sessions(project_id: int) -> List[Session]:
     """
     返回该项目的所有可用场次
 
@@ -97,12 +135,12 @@ async def get_available_sessions(project_id: int):
     rtn_list = []
     project_info = await get_project_info(project_id)
     for v in project_info["screen_list"]:
-        sess_obj = Session(id=int(v["id"]), start_time=int(v["start_time"]), formatted_time=str(v["name"]))
+        sess_obj = Session(id=v["id"], start_time=v["start_time"], formatted_time=v["name"])
         for t in v["ticket_list"]:
             sess_obj.ticket_list.append(
                 Ticket(
-                    id=int(t["id"]),
-                    price=int(t["price"]),
+                    id=t["id"],
+                    price=t["price"],
                     desc=t["desc"],
                     sale_start=t["sale_start"],
                     sale_end=t["sale_end"],
@@ -112,48 +150,60 @@ async def get_available_sessions(project_id: int):
     return rtn_list
 
 
-async def get_all_buyer_info(credential: Credential, rtn_buyer_info=False) -> Union[dict, List[BuyerInfo]]:
+async def get_ticket_buyer_list(credential: Credential) -> dict:
     """
     返回账号的全部身份信息
 
     Args:
         credential (Credential): 登录凭证
-        rtn_buyer_info (bool): 是否以BuyerInfo对象返回
 
     Returns:
-        dict or list[BuyerInfo]: 调用 API 返回的结果 或者 BuyerInfo对象列表
+        dict: 调用 API 返回的结果
     """
     credential.raise_for_no_sessdata()
     api = API["info"]["buyer_info"]
-    result = await Api(**api, credential=credential, ignore_code=True).result
-    if rtn_buyer_info:
-        rtn_list: List[BuyerInfo] = []
-        for v in result["list"]:
-            rtn_list.append(
-                BuyerInfo(personal_id=v["personal_id"], name=v["name"], tel=v["tel"], is_default=v["is_default"], org=v)
-            )
-        return rtn_list
-    return result
+    return await Api(**api, credential=credential).result
+
+
+async def get_all_buyer_info(credential: Credential) -> List[BuyerInfo]:
+    """
+    返回账号的全部身份信息
+
+    Args:
+        credential (Credential): 登录凭证
+
+    Returns:
+        list[BuyerInfo]: BuyerInfo对象列表
+    """
+    res = await get_ticket_buyer_list(credential)
+    return [BuyerInfo(**v) for v in res["list"]]
 
 
 class OrderTicket:
     """
-    购票类，购票流程所需的必要函数
+    购票类
+
+    Args:
+        credential (Credential): Credential 对象
+
+        buyer_info (BuyerInfo): BuyerInfo 对象
+
+        project_id (int): 展出id
+
+        session (Session): Session 对象
+
+        ticket (Ticket): Ticket 对象
     """
-
-    def __init__(self, credential: Credential, buyer_info: BuyerInfo, project_id: int, session: Session,
-                 ticket: Ticket):
-        """
-        Args:
-            credential (Credential): Credential 对象
-            buyer_info (BuyerInfo): BuyerInfo 对象
-            project_id (int): 展出id
-            session (Session): Session 对象
-            ticket (Ticket): Ticket 对象
-
-        """
+    def __init__(    
+        self,
+        credential: Credential,
+        buyer_list: List[BuyerInfo],
+        project_id: int,
+        session: Session,
+        ticket: Ticket
+    ):
         self.credential = credential
-        self.buyer_info = buyer_info
+        self.buyer_list = buyer_list
         self.project_id = project_id
         self.session = session
         self.ticket = ticket
@@ -167,29 +217,33 @@ class OrderTicket:
         """
         self.credential.raise_for_no_sessdata()
         api = API["info"]["token"]
-        payload = {"count": "1", "order_type": 1, "project_id": self.project_id, "screen_id": self.session.id,
-                   "sku_id": self.ticket.id}
-        return await Api(**api, credential=self.credential, ignore_code=True, no_csrf=True).update_data(
-            **payload).result
+        payload = {
+            "count": "1",
+            "order_type": 1,
+            "project_id": self.project_id,
+            "screen_id": self.session.id,
+            "sku_id": self.ticket.id
+        }
+        return await Api(**api, credential=self.credential).update_data(**payload).result
 
     async def create_order(self):
         """
         创建购买订单
+
+        Returns:
+            dict: 调用 API 返回的结果
         """
+        res = await self.get_token()
         payload = {
-            "buyer_info": [self.buyer_info.org].__str__().replace("'true'", "true").replace("'", '"'),
-            "buyer": self.buyer_info.name,
-            "tel": self.buyer_info.tel,
+            "buyer_info": json.dumps([b.__dict__ for b in self.buyer_list]),
             "count": 1,
-            "deviceId": "",
             "order_type": 1,
             "pay_money": self.ticket.price,
             "project_id": self.project_id,
             "screen_id": self.session.id,
             "sku_id": self.ticket.id,
-            "timestamp": int(round(time.time() * 1000)),
-            "token": (await self.get_token())["token"],
+            "timestamp": int(time.time() * 1000),
+            "token": res["token"],
         }
         api = API["operate"]["order"]
-        return await Api(**api, no_csrf=True, ignore_code=True, credential=self.credential).update_data(
-            **payload).result
+        return await Api(**api, credential=self.credential).update_params(project_id=self.project_id).update_data(**payload).result
