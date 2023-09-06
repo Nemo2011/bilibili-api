685523aa1a6646cf7d4e13f2bc3e6983087faf12
Drelf2018 <drelf2018@outlook.com>
2023-09-07 02:43:21 +0800

    remove: 移除测试函数

--- tests/test_show.py
+++ tests/test_show.py
@@ -13,11 +13,3 @@ async def test_a_get_all_buyer_info():
 
 async def test_b_get_available_sessions():
     return await show.get_available_sessions(PROJECT_ID)
-
-
-async def test_c_order_ticket():
-    buyers = (await test_a_get_all_buyer_info())
-    session = (await test_b_get_available_sessions())[0]
-    ticket = session.ticket_list[0]
-    ot = show.OrderTicket(get_credential(), buyers, buyers[0], PROJECT_ID, session, ticket)
-    return await ot.create_order()
