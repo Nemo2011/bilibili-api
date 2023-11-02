4db30f4670fa4ca0781271d3627758aa94c9d635
yueyatianchong <yueyawochong@qq.com>
2023-11-02 14:36:42 +0800

    fix: 修复无法获取弹幕和历史弹幕的bug

--- bilibili_api/ass.py
+++ bilibili_api/ass.py
@@ -192,8 +192,7 @@ async def make_ass_file_danmakus_protobuf(
             if cid is None:
                 if page is None:
                     raise ArgsException("page_index 和 cid 至少提供一个。")
-                # type: ignore
-                cid = await v._Video__get_page_id_by_index(page)
+                # cid = await v._Video__get_page_id_by_index(page) # type: ignore
         try:
             info = await v.get_info()
         except:
@@ -274,7 +273,7 @@ async def make_ass_file_danmakus_xml(
             if cid is None:
                 if page is None:
                     raise ArgsException("page_index 和 cid 至少提供一个。")
-                cid = await v._Video__get_page_id_by_index(page)  # type: ignore
+                # cid = await v._Video__get_page_id_by_index(page)  # type: ignore
         try:
             info = await v.get_info()
         except:
