902b73a94d05fe782ecc6fca9a0a0531695dbd14
yfh01 <987751945@qq.com>
2023-12-21 14:24:34 +0800

    feat: 新增fake-useragent用于指定或随机生成UA

--- bilibili_api/utils/network.py
+++ bilibili_api/utils/network.py
@@ -38,9 +38,10 @@ OE = [
     46, 47, 18, 2, 53, 8, 23, 32, 15, 50, 10, 31, 58, 3, 45, 35, 27, 43, 5, 49, 33, 9, 42, 19, 29, 28, 14, 39, 12, 38, 41, 13,
     37, 48, 7, 16, 24, 55, 40, 61, 26, 17, 0, 1, 60, 51, 30, 4, 22, 25, 54, 21, 56, 59, 6, 63, 57, 62, 11, 36, 20, 34, 44, 52,
 ]
+ua = UserAgent()
 # 使用 Referer 和 UA 请求头以绕过反爬虫机制
 HEADERS = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.54",
+    "User-Agent": ua.fallback,
     "Referer": "https://www.bilibili.com",
 }
 API = get_api("credential")
