acace35e7e6aaa49081ce8f4540050a9dfde5519
TimG233 <54853713+TimG233@users.noreply.github.com>
2024-03-19 21:08:22 -0400

    refactor: remove unused & unneeded credential for get_real_url

--- bilibili_api/utils/short.py
+++ bilibili_api/utils/short.py
@@ -3,28 +3,24 @@ bilibili_api.utils.short
 
 一个很简单的处理短链接的模块，主要是读取跳转链接。
 """
-from typing import Optional
 
 from .. import settings
 from .credential import Credential
 from .network import get_session, get_aiohttp_session
 
 
-async def get_real_url(short_url: str, credential: Optional[Credential] = None) -> str:
+async def get_real_url(short_url: str) -> str:
     """
     获取短链接跳转目标，以进行操作。
 
     Args:
         short_url(str): 短链接。
 
-        credential(Credential \| None): 凭据类。
-
     Returns:
         目标链接（如果不是有效的链接会报错）
 
         返回值为原 url 类型
     """
-    credential = credential if credential else Credential()
 
     try:
         if settings.http_client == settings.HTTPClient.HTTPX:
