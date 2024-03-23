7d215a99105b2b32255f174b97520029aecbcc7d
z0z0r4 <z0z0r4@outlook.com>
2024-03-23 23:43:44 +0800

    chore: 添加参数
    不需要 try，不成功就应该报错

--- bilibili_api/utils/short.py
+++ bilibili_api/utils/short.py
@@ -3,6 +3,7 @@ bilibili_api.utils.short
 
 一个很简单的处理短链接的模块，主要是读取跳转链接。
 """
+
 from typing import Optional
 
 from .. import settings
@@ -20,7 +21,7 @@ async def acquire_buvid(credential: Optional[Credential] = None) -> str:
     从Credential中取出buvid3，若不存在则通过spi获取buvid，若都不存在则随机生成一个的buvid3。
 
     Args:
-        credential(Credential \| None): 凭据类。
+        credential(Credential | None): 凭据类。
 
     Returns:
         buvid3的字符串
@@ -28,81 +29,118 @@ async def acquire_buvid(credential: Optional[Credential] = None) -> str:
 
     # return given buvid3 if possible
     if credential:
-        buvid3 = credential.get_cookies()['buvid3']
+        buvid3 = credential.get_cookies()["buvid3"]
 
         if buvid3 is not None:
             return buvid3
 
     # use spi to get buvid3
     try:
-        return (await get_spi_buvid())['data']['b_3']
+        return (await get_spi_buvid())["data"]["b_3"]
 
     except KeyError:  # if data or b_3 does not exist by spi
         pass
 
     # random generation if spi is not possible
-    buvid3_pattern = '8-4-4-4-17'  # current buvid3 char-length pattern, might be changed later
-    parts = buvid3_pattern.split('-')
+    buvid3_pattern = (
+        "8-4-4-4-17"  # current buvid3 char-length pattern, might be changed later
+    )
+    parts = buvid3_pattern.split("-")
 
-    buvid3_rand_gen = ["".join(random.choices(string.digits + string.ascii_letters, k=int(part))) for part in parts]
+    buvid3_rand_gen = [
+        "".join(random.choices(string.digits + string.ascii_letters, k=int(part)))
+        for part in parts
+    ]
 
     return "-".join(buvid3_rand_gen) + "infoc"
 
 
-async def get_short_url(real_url: str, credential: Optional[Credential] = None) -> str:
+async def get_short_url(
+    oid: Optional[int] = None,
+    share_content: Optional[str] = None,
+    share_title: Optional[str] = None,
+    share_origin: Optional[str] = "vinfo_share",
+    share_mode: Optional[int] = 3,
+    share_id: Optional[str] = "public.webview.0.0.pv",
+    platform: Optional[str] = "android",
+    mobi_app: Optional[str] = "android",
+    panel_type: Optional[int] = 1,
+    # regex_real_url: Optional[bool] = False,
+    credential: Optional[Credential] = None,
+) -> str:
     """
-    获取目标链接的短链接。支持bilibili.com的相关链接。
+    获取目标链接的短链接。支持 bilibili.com 的相关链接。
+
+    尽管目标链接可能不存在，但仍然会生成短链接。
+
+    相同的目标链接重复调用此方法会获得不同的短链接。
 
     Args:
-        real_url(str): 目标链接。
+        oid            (int | None): 内容 oid。
 
-        credential(Credential \| None): 凭据类。
+        share_content  (str | None): 分享内容。
+
+        share_title    (str | None): 分享标题。
+
+        share_origin   (str | None): 分享来源。
+
+        share_mode     (int | None): 分享模式。
+
+        share_id       (str | None): 分享 id。
+
+        platform       (str | None): 平台。
+
+        mobi_app       (str | None): 移动端应用。
+
+        panel_type     (int | None): 面板类型。
+
+        credential   (Credential | None): 凭据类。
 
     Returns:
-        目标链接的b23.tv短链接。
-
-        尽管目标链接可能不存在，但仍然会生成短链接。
-
-        相同的目标链接重复调用此方法会获得不同的短链接。
+        str: 目标链接的 b23.tv 短链接信息 (不一定为单独 URL)
     """
+    # 应该具体为检测 oid 类型
+    # if regex_real_url:
+    #     # validate the starting part of url
+    #     url_start_pattern = re.compile(pattern=r"^https?:\/\/(?:www\.)?bilibili\.com")
 
-    # validate the starting part of url
-    url_start_pattern = re.compile(pattern=r'^https?:\/\/(?:www\.)?bilibili\.com')
+    #     if not re.match(pattern=url_start_pattern, string=share_content):
+    #         raise ArgsException(
+    #             msg=f"提供的 {share_content} 不符合格式。\
+    #                                 支持的格式为 bilibili.com 的相关链接并含有 http 或 https 协议。"
+    #         )
 
-    if not re.match(pattern=url_start_pattern, string=real_url):
-        raise ArgsException(msg=f"提供的real_url {real_url} 不符合格式。"
-                                f"支持的格式为bilibili.com的相关链接并含有http或https协议。")
+    post_data = {
+        "build": 7300400,
+        "buvid": await acquire_buvid(credential=credential),
+        "oid": oid,
+        "share_title": share_title,
+        "share_content": share_content,
+        "share_origin": share_origin,
+        "mobi_app": mobi_app,
+        "panel_type": panel_type,
+        "platform": platform,
+        "share_id": share_id,
+        "share_mode": share_mode,
+    }
 
-    # POST request
-    try:
-        post_data = {
-            'build': 999999999,  # number > 5.5M should work
-            'buvid': await acquire_buvid(credential=credential),
-            'oid': real_url,
-            'platform': random.choice(['android', 'ios']),
-            'share_channel': 'COPY',
-            'share_id': 'public.webview.0.0.pv',
-            'share_mode': 3
-        }
+    api_url = "https://api.biliapi.net/x/share/click"
 
-        api_url = 'https://api.biliapi.net/x/share/click'
+    if settings.http_client == settings.HTTPClient.HTTPX:
+        resp_content = (
+            await get_session().post(url=api_url, headers=HEADERS, data=post_data)
+        ).json()
+    else:
+        resp = await get_aiohttp_session().post(
+            url=api_url, data=post_data, headers=HEADERS
+        )
+        resp_content = await resp.json()
 
-        if settings.http_client == settings.HTTPClient.HTTPX:
-            resp_content = (await get_session().post(url=api_url, headers=HEADERS, data=post_data)).json()
-        else:
-            resp = await get_aiohttp_session().post(
-                url=api_url, data=post_data, headers=HEADERS
-            )
-            resp_content = await resp.json()
+    # the 'content' sometimes will not be in the returned content due to build version, real_url, or buvid (rarely)
+    if "content" not in resp_content["data"]:
+        raise ApiException(msg="生成短链接失败。")
 
-        # the 'content' sometimes will not be in the returned content due to build version, real_url, or buvid (rarely)
-        if 'content' not in resp_content['data']:
-            raise ApiException(msg="生成短链接失败。若提供的目标链接格式确认正确，请反馈此bug以更新相应params")
-
-        return resp_content['data']['content']
-
-    except Exception as e:
-        raise e
+    return resp_content["data"]["content"]
 
 
 async def get_real_url(short_url: str) -> str:
