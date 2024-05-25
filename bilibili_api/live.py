36dfe95e4c4a2299f8ad4597b922786558203005
TZFC <tianzifangchen@gmail.com>
2024-05-25 03:31:46 -0400

    增加充电表情id的格式说明

--- bilibili_api/live.py
+++ bilibili_api/live.py
@@ -530,6 +530,8 @@ class LiveRoom:
 
         Args:
             danmaku (Danmaku): 弹幕类， text为表情包 id
+                房间表情形如 room_23596840_10003
+                充电表情形如 upower_[UPOWER_36265198_震惊]
 
         Returns:
             dict: 调用 API 返回的结果
