20972188a73b978f51ad9b1ee7ea3499e75ed11e
Nemo2011 <yimoxia@outlook.com>
2024-12-29 21:21:43 +0800

    feat: emoji.get_emoji_detail / emoji.get_all_emoji & update emote.json

--- bilibili_api/emoji.py
+++ bilibili_api/emoji.py
@@ -5,21 +5,64 @@ bilibili_api.emoji
 """
 
 from .utils.utils import get_api
-from .utils.network import Api
+from .utils.network import Api, Credential
+
+from typing import Union, List
 
 API = get_api("emoji")
 
 
-async def get_emoji_list(business: str = "reply") -> dict:
+async def get_emoji_list(
+    business: str = "reply", credential: Credential = None
+) -> dict:
     """
     获取表情包列表
 
     Args:
+        business   (str): 使用场景, reply / dynamic
+        credential (Credential): 登录凭证. Defaults to None.
+
+    Returns:
+        dict: 调用 API 返回的结果
+    """
+    credential = credential if credential else Credential()
+    api = API["list"]
+    params = {"business": business}
+    return await Api(**api, credential=credential).update_params(**params).result
+
+
+async def get_emoji_detail(id: Union[int, List[int]], business: str = "reply") -> dict:
+    """
+    获取表情包详情
+
+    Args:
+        id       (Union[int, List[int]]): 表情包 id，可通过 `get_emoji_list` 或 `get_all_emoji` 查询。
         business (str): 使用场景, reply / dynamic
 
     Returns:
         dict: 调用 API 返回的结果
     """
-    api = API["list"]
-    params = {"business": business}
+    api = API["detail"]
+    params = {
+        "ids": ",".join([str(i) for i in id]) if isinstance(id, list) else id,
+        "business": business,
+    }
     return await Api(**api).update_params(**params).result
+
+
+async def get_all_emoji(business: str = "reply", credential: Credential = None) -> dict:
+    """
+    获取所有表情包
+
+    Args:
+        business   (str): 使用场景, reply / dynamic
+        credential (Credential): 登录凭证. Defaults to None.
+
+    Returns:
+        dict: 调用 API 返回的结果
+    """
+    credential = credential if credential else Credential()
+    credential.raise_for_no_sessdata()
+    api = API["all"]
+    params = {"business": business}
+    return await Api(**api, credential=credential).update_params(**params).result
---
+++ scripts/get_emoji_list.py
@@ -0,0 +1,37 @@
+import json
+
+import httpx
+
+from bilibili_api.utils.network import HEADERS
+from bilibili_api import Credential, sync, emoji
+
+import time
+
+PATH_TO_CREDENTIAL_JSON = "test1.json"
+
+data = json.load(open(PATH_TO_CREDENTIAL_JSON, "r"))
+cred = Credential(
+    sessdata=data["SESSDATA"],
+    bili_jct=data["bili_jct"],
+    buvid3=data["buvid3"],
+    ac_time_value=data["ac_time_value"],
+)
+
+res1 = sync(emoji.get_all_emoji(credential=cred))
+
+
+ids = []
+for pkg in res1["all_packages"][1:]:
+    ids.append(pkg["id"])
+
+data = {}
+for seg in range(0, len(ids), 10):
+    print(f"{seg} ~ {seg + 9}")
+    res2 = sync(emoji.get_emoji_detail(ids[seg : seg + 10]))
+    time.sleep(0.8)
+    for pkg in res2["packages"]:
+        if pkg.get("emote"):
+            for emj in pkg["emote"]:
+                data[emj["id"]] = emj["text"]
+
+json.dump(data, open("emote.json", "w+"), ensure_ascii=False, indent=4)
