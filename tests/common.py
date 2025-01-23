ed3cfdac6f676eeb1dcbf0352c1d8b7693ae392e
Nemo2011 <yimoxia@outlook.com>
2025-01-23 12:36:54 +0800

    chore: remove login_func & login

--- tests/common.py
+++ tests/common.py
@@ -1,5 +1,5 @@
 
-from bilibili_api import Credential, login
+from bilibili_api import Credential
 import os
 
 from bilibili_api import Credential
@@ -12,16 +12,6 @@ def get_credential():
     BILI_DEDEUSERID = os.getenv("BILI_DEDEUSERID")
 
     if not BILI_SESSDATA or not BILI_CSRF or not BILI_BUVID3 or not BILI_DEDEUSERID:
-        print(Exception("缺少环境变量"))
-        cred = login.login_with_qrcode_term()
-        os.environ["BILI_SESSDATA"] = cred.sessdata
-        os.environ["BILI_CSRF"] = cred.bili_jct
-        os.environ["BILI_BUVID3"] = cred.buvid3
-        os.environ["BILI_DEDEUSERID"] = cred.dedeuserid
-        BILI_SESSDATA = os.getenv("BILI_SESSDATA")
-        BILI_CSRF = os.getenv("BILI_CSRF")
-        BILI_BUVID3 = os.getenv("BILI_BUVID3")
-        BILI_DEDEUSERID = os.getenv("BILI_DEDEUSERID")
-    
+        raise Exception("缺少环境变量")
+
     return Credential(BILI_SESSDATA, BILI_CSRF, BILI_BUVID3, BILI_DEDEUSERID)
-    
