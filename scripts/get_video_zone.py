e0084aca297b9dc2fa7470a3897acc2615d7bfde
Nemo2011 <yimoxia@outlook.com>
2025-01-30 22:02:24 +0800

    fix: get_video_zone.py

--- scripts/get_video_zone.py
+++ scripts/get_video_zone.py
@@ -7,12 +7,12 @@ from bilibili_api.utils.network import HEADERS
 data = httpx.get(
     "https://api.bilibili.com/x/kv-frontend/namespace/data?appKey=333.1339&nscode=2",
     headers=HEADERS,
-).json()["data"]
+).json()["data"]["data"]
 
 res = []
 
-for key, value in data:
-    if str(key).startswith("channel.") and key != "channel.all":
-        res.append(value)
+for key, value in data.items():
+    if str(key).startswith("channel_list.") and key != "channel_list.all":
+        res.append(json.loads(value))
 
-print(json.dumps(res, indent=4, ensure_ascii=False))
+print(json.dumps(res, indent=2, ensure_ascii=False))
