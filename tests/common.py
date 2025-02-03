acf450218d2a7381ea2d909ef4a07704c265c350
Nemo2011 <yimoxia@outlook.com>
2025-02-03 10:58:51 +0800

    tests: don't rely on user-provided buvid3

--- tests/common.py
+++ tests/common.py
@@ -11,7 +11,7 @@ def get_credential():
     BILI_BUVID3 = os.getenv("BILI_BUVID3")
     BILI_DEDEUSERID = os.getenv("BILI_DEDEUSERID")
 
-    if not BILI_SESSDATA or not BILI_CSRF or not BILI_BUVID3 or not BILI_DEDEUSERID:
+    if not BILI_SESSDATA or not BILI_CSRF or not BILI_DEDEUSERID:
         raise Exception("缺少环境变量")
 
     return Credential(BILI_SESSDATA, BILI_CSRF, BILI_BUVID3, BILI_DEDEUSERID)
